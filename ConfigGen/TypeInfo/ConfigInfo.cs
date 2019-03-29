da53c26871094fd89645f9b2750e9880e1ef7d28
瞎蹦跶 <609477964@qq.com>
2019-03-29 19:55:51 +0800

    rowcolumn
    data start
    typeinfo start
    io start

---
+++ ConfigGen/TypeInfo/ConfigInfo.cs
@@ -0,0 +1,33 @@
+﻿using ConfigGen.Data;
+using ConfigGen.Description;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConfigGen.TypeInfo
+{
+    public class ConfigInfo
+    {
+        private static Dictionary<string, ConfigInfo> _configs = new Dictionary<string, ConfigInfo>();
+        public static Dictionary<string, ConfigInfo> Configs { get { return _configs; } }
+        public static ConfigInfo Get(string fullName)
+        {
+            return IsConfig(fullName) ? _configs[fullName] : null;
+        }
+        public static bool IsConfig(string fullName)
+        {
+            return _configs.ContainsKey(fullName);// && _configs[fullName].
+        }
+
+        private ClassDes _des;
+        private string _namespace;
+        private string _fullName;
+        private string _xmlDir;
+        private FList _data;
+        private string[] _inputFiles;
+        private string _outputFile;
+        private readonly HashSet<string> _groups;
+
+    }
+}
---
+++ ConfigGen/TypeInfo/GroupInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConfigGen.TypeInfo
+{
+    public class GroupInfo
+    {
+        private HashSet<string> _groups = new HashSet<string>();
+
+        public GroupInfo(string group)
+        {
+
+        }
+
+        public bool CheckGroup(HashSet<string> gs)
+        {
+            return _groups.con
+        }
+
+    }
+}
